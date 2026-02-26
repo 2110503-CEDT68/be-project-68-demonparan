41f62e7aac0108fc07d1660fdcb6267e93391b23
Teejutha <ja154k@gmail.com>
2026-02-26 14:10:00 +0700

    models middleware package

---
+++ models/Dentist.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const DentistSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add dentist name'],
+      trim: true,
+      maxlength: [50, 'Name cannot be more than 50 characters']
+    },
+    experience: {
+      type: Number,
+      required: [true, 'Please add years of experience']
+    },
+    expertise: {
+      type: String,
+      required: [true, 'Please add area of expertise']
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+// Reverse populate (Dentist -> Appointments)
+DentistSchema.virtual('appointments', {
+  ref: 'Appointment',
+  localField: '_id',
+  foreignField: 'dentist',
+  justOne: false
+});
+
+module.exports = mongoose.model('Dentist', DentistSchema);
